cb56e1871fc6744fdb76afcc4e2a2b32b06014d7
Viktoria <zaharova181114@gmail.com>
2021-12-30 23:41:25 +0300

    add blocks

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,10 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$('.cases-slider').slick({
+    slidesToShow: 1,
+    variableWidth: true,
+    appendArrows: '.cases-slider__nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
 });
+
+$('[name="phone"]').mask('+7(999) 999-99-99');
+
